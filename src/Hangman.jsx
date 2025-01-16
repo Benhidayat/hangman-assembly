299680801846672fb10d26be67acc4e234679c70
benhidayat <98013492+Benhidayat@users.noreply.github.com>
2025-01-16 13:58:28 +0700

    created p tag for descriptions
    fixing layout by putting h1 p inside a container

--- src/Hangman.jsx
+++ src/Hangman.jsx
@@ -4,7 +4,11 @@ import './Hangman.css';
 const Hangman = () => {
   return (
     <main>
-      <h1>Assembly: Endgame</h1>
+        <div className="title__container">
+            <h1>Assembly: Endgame</h1>
+            <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+        </div>
+
     </main>
   )
 }
