06337f68016185ad50c9f99c0d4a37a79c3ba5a2
benhidayat <98013492+Benhidayat@users.noreply.github.com>
2025-01-17 12:41:40 +0700

    added language lists and letter section

--- src/Hangman.jsx
+++ src/Hangman.jsx
@@ -1,17 +1,61 @@
-import React from 'react';
+import { useState } from 'react';
 import './Hangman.css';
+import { languages } from './language.js';
 
 const Hangman = () => {
+    const [currentWord, setCurrentWord] = useState('react');
+
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+    // keyboard
+    const keyboardElements = [...alphabet].map(letter => (
+        <button key={letter}>{letter.toUpperCase()}</button>
+    ))
+
+    // letter
+    const wordElements = [...currentWord].map((letter, index) => {
+        return  (
+            <span className='letter' key={index}>{letter.toUpperCase()}</span>
+        )
+    })
+
+
+    //language
+    const langElements = languages.map(langObj => {
+
+        const styles = {
+            backgroundColor: langObj.backgroundColor,
+            color: langObj.color
+        }
+
+        return (
+            <div style={styles}
+                 key={langObj.name}
+            >
+                {langObj.name}
+            </div>
+        )
+    });
+
   return (
     <main>
         <header className="title__container">
             <h1>Assembly: Endgame</h1>
             <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
         </header>
-        <h2 className="game__status">
+        <section className="game__status">
             <h2>You Win!</h2>
             <p>Well done! 🎉</p>
-        </h2>
+        </section>
+        <section className='language__section'>
+            {langElements}
+        </section>
+        <section className='word__section'>
+            {wordElements}
+        </section>
+        <section className="keyboard__section">
+            {keyboardElements}
+        </section>
     </main>
   )
 }
